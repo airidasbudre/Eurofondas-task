8d1d3e1fbcd9f29bc690defcdbd70926321a4c0d
budreckis <a.budreckis@umina.eu>
2024-12-04 11:59:57 +0200

    Add custom addons

---
+++ addons/library/template/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "",
+    'summary': """
+
+    """,
+    "author": "Airidas Budreckis",
+    "website": "",
+    "category": "Partner",
+    "version": "13.0.1.0.0",
+    'depends': ['base'],
+    "assets": {},
+    'data': [
+        
+    ],
+    "license": "LGPL-3",
+}
